ec95aecd5dd2dad8e25cd3a650385bc58a334894
Tssongy <taemine.song@gmail.com>
2022-02-02 09:46:47 +1100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Breakdown of the logic to be implemented for the game of tic tac toe
+
+// 1. An input (play) for each of the two players is required and is to fill up one space on the board
+// 2. After an input (play), check if any one row OR column OR diagonal has been filled by one of the players
+// 3. If any one row OR column OR diagonal has been filled by a player, pause the game and display a message indicating the victory
+// 4. If no row, column, and diagonal has been filled by one player, initiate the next player's turn
+// 5. If there are no spaces left on the board (i.e all spaces have been filled by players' inputs), declare DRAW
+// 6. Declare a counter variable that increments with each game played to let users know how many games have been played.
+// 7. Declare a counter variable for each player that increments with their winnings respectively.
+// 8. When the game ends, prompt the user if they would like to play again -> if YES, reset the board.
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Project 1: The Game (tic tac toe)
+
+My first project with General Assembly as part of Software Engineering Immersive with the following deliverables:
+
+- Render a game in the browser
+- Switch turns between more than one player
+- Design logic for winning & visually display which player won
+- Include separate HTML / CSS / JavaScript files
+- Stick with KISS (Keep It Simple Stupid) principles
+- Use Javascript for DOM manipulation
+- Deploy your game online, where the rest of the world can access it
+- Use semantic markup for HTML and CSS (adhere to best practices)
+
+And some extra features!
